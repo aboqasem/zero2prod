2a448e2438b4ae1b47ba015b0b7eb4ce0e1b79d9
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2023-04-15 07:57:44 +0800

    feat: simple `/subscriptions` route and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::fmt::Formatter;
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
@@ -6,10 +7,37 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct SubscriptionData {
+    name: String,
+    email: String,
+}
+
+impl std::fmt::Display for SubscriptionData {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{ name: {}, email: {} }}", self.name, self.email)
+    }
+}
+
+#[derive(serde::Serialize)]
+struct SubscriptionResponse {
+    message: String,
+}
+
+async fn subscribe(data: web::Form<SubscriptionData>) -> impl Responder {
+    HttpResponse::Created().json(SubscriptionResponse {
+        message: format!("Received {data}."),
+    })
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
