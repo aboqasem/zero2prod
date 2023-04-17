8b9e7a20f6696fa1b29f95d537cbc84571fb605f
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2023-04-17 08:11:37 +0800

    fix: `migrate` script instead of on app start up

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-use sqlx::PgPool;
 use std::net::TcpListener;
 
 use zero2prod::settings::SETTINGS;
-use zero2prod::startup::{migrate_db, run};
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let pool = PgPool::connect(&SETTINGS.database.url())
-        .await
-        .expect("Failed to connect to Postgres");
-
-    migrate_db(&pool).await;
-
     let port = SETTINGS.port;
     run(TcpListener::bind(format!("127.0.0.1:{port}"))
         .unwrap_or_else(|_| panic!("Failed to bind to port {port}")))?
--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,8 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
-use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
-
-pub async fn migrate_db(pool: &PgPool) {
-    sqlx::migrate!()
-        .run(pool)
-        .await
-        .expect("Failed to run migrations")
-}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
