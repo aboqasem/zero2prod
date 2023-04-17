c04738d776d5b12bafbee095b2ddaf8053b84993
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2023-04-18 00:09:16 +0800

    feat(subscriptions): inser user to DB

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,37 @@
 use std::fmt::Formatter;
 
 use actix_web::{web, HttpResponse, Responder};
+use sqlx::PgPool;
 
-pub async fn subscribe(data: web::Form<SubscriptionData>) -> impl Responder {
-    HttpResponse::Created().json(SubscriptionResponse {
-        message: format!("Received {data}."),
-    })
+pub async fn subscribe(
+    data: web::Form<SubscriptionData>,
+    pool: web::Data<PgPool>,
+) -> impl Responder {
+    let insertion = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions
+        VALUES ($1, $2, $3, $4)
+        "#,
+        uuid::Uuid::new_v4(),
+        data.email,
+        data.name,
+        chrono::Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await;
+
+    match insertion {
+        Ok(_) => HttpResponse::Created().json(SubscriptionResponse {
+            message: "Subscribed!".to_string(),
+        }),
+        Err(error) => {
+            eprintln!("Failed to subscribe: {}", error);
+
+            HttpResponse::InternalServerError().json(SubscriptionResponse {
+                message: "Failed to subscribe".to_string(),
+            })
+        }
+    }
 }
 
 #[derive(serde::Deserialize)]
