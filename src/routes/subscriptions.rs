308fecd30fab228d0bbe30d1a5049386edf575dc
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2023-06-11 02:14:23 +0800

    feat(subscriptions): send static email

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,35 +2,47 @@ use actix_web::{web, HttpResponse, Responder};
 use sqlx::PgPool;
 
 use crate::domain::{RawSubscriber, Subscriber, SubscriptionStatus};
+use crate::email::{EmailClient, EmailData};
 
 #[tracing::instrument(
-name = "Adding new subscriber",
-skip(subscriber, pool),
-fields(
-subscriber.email,
-subscriber.name,
-),
+    name = "Adding new subscriber",
+    skip_all,
+    fields(
+        subscriber.email,
+        subscriber.name,
+    ),
 )]
 pub async fn subscribe(
     subscriber: web::Form<RawSubscriber>,
     pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
 ) -> impl Responder {
     let subscriber: Subscriber = match subscriber.0.try_into() {
         Ok(s) => s,
         Err(e) => return HttpResponse::BadRequest().json(SubscriptionResponse { message: e }),
     };
 
-    match insert_subscriber(&subscriber, &pool).await {
-        Ok(_) => HttpResponse::Created().json(SubscriptionResponse {
+    let email_data = EmailData {
+        to: subscriber.email.clone(),
+        subject: "Welcome!".into(),
+        content: "Welcome to my newsletter!".into(),
+        content_type: "text/plain".into(),
+    };
+
+    if insert_subscriber(&subscriber, &pool).await.is_ok()
+        && email_client.send(&email_data).await.is_ok()
+    {
+        return HttpResponse::Created().json(SubscriptionResponse {
             message: "Subscribed!".into(),
-        }),
-        Err(_) => HttpResponse::InternalServerError().json(SubscriptionResponse {
-            message: "Failed to subscribe".into(),
-        }),
+        });
     }
+
+    HttpResponse::InternalServerError().json(SubscriptionResponse {
+        message: "Failed to subscribe".into(),
+    })
 }
 
-#[tracing::instrument(name = "Inserting subscriber to DB", skip(subscriber, pool))]
+#[tracing::instrument(name = "Inserting subscriber to DB", skip_all)]
 async fn insert_subscriber(subscriber: &Subscriber, pool: &PgPool) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
